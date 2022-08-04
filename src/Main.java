c0e4be3e151a867ca358b3eb79c912303c49c329
Tariel <tarielkazakbaevnahuy@gmail.com>
2022-08-04 18:03:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+//        create table "HomeWork_44".visitors
+//                (
+//                        fullname varchar,
+//                        id serial not null,
+//                "dateOfBirth" TIMESTAMP,
+//                gender varchar
+//);
+//        create unique index visitors_id_uindex
+//        on "HomeWork_44".visitors(id);
+//        alter table "HomeWork_44".visitors
+//        add constraint visitors_pk
+//        primary key (id);
+//        -----
+//                create table "HomeWork_44".visits
+//                (
+//                        id serial not null
+//        constraint visits_products_id_fk
+//        references "HomeWork_44".visitors,
+//                column_2 int,
+//        visitor_id int,
+//        date timestamp,
+//        trainer varchar
+//);
+//        create unique index visits_id_uindex
+//        on "HomeWork_44".visits(id);
+//        alter table "HomeWork_44".visits
+//        add constraint visits_pk
+//        primary key (id);
+//        ----
+//                insert into "HomeWork_44".visitors(fullname, id, "dateOfBirth", gender)
+//        values('ads', 1, '01.01.2022 00:00:00.000000', 'male');
+//
+//        select
+//        from "HomeWork_44".visits v.fullname, v.trainer, v.date
+//        inner join "HomeWork_44".visitors vv on v.visitor_id = vv.id
+//        order by v.date
+    }
+}
